ade20fc980daf7120932aa82f79b3867ef100261
pm-white <philmwhite@pm.me>
2025-03-15 17:06:37 -0400

    set up db writing on Add page

--- routes/add.js
+++ routes/add.js
@@ -8,7 +8,24 @@ router.get("/", async function (req, res, next) {
 });
 
 router.post("/", (req, res, next) => {
-  console.log(req.body);
+  db.none("insert into books values($1, $2, $3, $4, $5, $6, $7, $8, $9)", [
+    req.body.title,
+    req.body.first_name,
+    req.body.last_name,
+    req.body.year_published,
+    req.body.type,
+    req.body.format,
+    req.body.num_pages,
+    req.body.start,
+    req.body.end,
+  ])
+    .then(() => {
+      res.status(200).redirect("/add?=message=Success!");
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).redirect("/add?=message=Error uploading book.");
+    });
 });
 
 module.exports = router;
