aa0d150524bec305087194ad8d0995d3c965b573
pm-white <philmwhite@pm.me>
2026-01-01 15:15:27 -0500

    more reorganizing

--- backend/built/db/connection.js
+++
@@ -1,6 +0,0 @@
-import pgPromise from "pg-promise";
-var pgp = pgPromise();
-var base = "".concat(process.env.DB_USERNAME, ":").concat(process.env.DB_PASSWORD, "@").concat(process.env.DB_HOST, ":").concat(process.env.DB_PORT, "/").concat(process.env.DB_NAME);
-export var db = pgp(process.env.NODE_ENV === "production"
-    ? "postgres://".concat(base, "?sslmode=require")
-    : "postgres://".concat(base));
