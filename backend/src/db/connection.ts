b93daba699c9e19054847940aba2c93c005499be
pm-white <philmwhite@pm.me>
2026-05-03 12:53:00 -0400

    revise index.ts and get main query working

--- backend/src/db/connection.ts
+++ backend/src/db/connection.ts
@@ -1,3 +1,4 @@
 import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+export const db = drizzle(new Pool({ connectionString: process.env.DB_URL }));
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,7 @@
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
 import { Book } from "./types.js";
-import {
-  getCompletedBooks,
-  getInProgressBooks,
-  getBacklogBooks,
-} from "./model.js";
+import { getBooks, testQuery } from "./model.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,45 +9,11 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get(
-  "/completed",
+  "/get-books",
   expressAsyncHandler(async (req, res) => {
     let books: Book[];
     try {
-      books = await getCompletedBooks();
-      if (books.length >= 1) {
-        res.status(200).json(books);
-      } else {
-        res.status(404).json({ Error: "No boooks in tracker." });
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  }),
-);
-
-app.get(
-  "/in-progress",
-  expressAsyncHandler(async (req, res) => {
-    let books: Book[];
-    try {
-      books = await getInProgressBooks();
-      if (books.length >= 1) {
-        res.status(200).json(books);
-      } else {
-        res.status(404).json({ Error: "No boooks in tracker." });
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  }),
-);
-
-app.get(
-  "/backlog",
-  expressAsyncHandler(async (req, res) => {
-    let books: Book[];
-    try {
-      books = await getBacklogBooks();
+      books = await getBooks();
       if (books.length >= 1) {
         res.status(200).json(books);
       } else {
